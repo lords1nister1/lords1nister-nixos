851cf758309fd77da79700996d5eeacb13b34063
lords1nister1 <youubepro@gmail.com>
2026-02-13 21:36:33 +0100

    Networking stuff: profiles

--- configuration.nix
+++ configuration.nix
@@ -1,10 +1,5 @@
-
-# Rebuild: nixswitch (broken) Actually rebuild: cswitch
-# 
-#
-
-
 { config, pkgs, ... }:
+
 {
   imports = [
     ./hardware-configuration.nix
@@ -21,7 +16,6 @@
     };
   };
 
-
   nixpkgs.config.allowUnfree = true;
 
   boot.loader.systemd-boot.enable = false;
@@ -29,18 +23,43 @@
   boot.loader.grub.enable = true;
   boot.loader.grub.efiSupport = true;
   boot.loader.grub.device = "nodev";
-  
 
   networking.hostName = "nixos";
   networking.networkmanager.enable = true;
-  
+  networking.networkmanager.ensureProfiles.profiles = 
+  {
+   home-wifi = {
+    connection = {
+     id = "ARRIS-022C";
+     type = "wifi";
+     autoconnect = true;
+  };
+
+  wifi = {
+    ssid = "ARRIS-022C";
+    mode = "infrastructure";
+    };
+
+  wifi-security = {
+   key-mgmt = "wpa-psk";
+   psk = "C420252B5187245B";
+  };
+
+ ipv4 = {
+  method = "auto";
+ };
+
+ ipv6 = {
+  method = "auto";
+  };
+ };
 
 
 
-
-
+};
 
   time.timeZone = "Europe/Vienna";
+
   i18n.defaultLocale = "en_US.UTF-8";
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "de_AT.UTF-8";
@@ -59,8 +78,7 @@
   services.desktopManager.plasma6.enable = true;
   services.flatpak.enable = true;
 
-systemd.services.NetworkManager-wait-online.enable = false;
-
+  systemd.services.NetworkManager-wait-online.enable = false;
 
   services.xserver.xkb = {
     layout = "de";
@@ -70,11 +88,11 @@ systemd.services.NetworkManager-wait-online.enable = false;
   console.keyMap = "de";
 
   services.printing.enable = true;
+
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
- 
 
-   services.pipewire = {
+  services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
@@ -90,57 +108,39 @@ systemd.services.NetworkManager-wait-online.enable = false;
     ];
   };
 
-
   programs.firefox.enable = false;
 
-
   environment.systemPackages = with pkgs; [
-  pkgs.kitty
-  pkgs.fastfetch
-  pkgs.fzf
-  pkgs.cava
-  pkgs.gnome-secrets
-  pkgs.neofetch
-  pkgs.starship
-  pkgs.clock-rs
-  pkgs.peaclock
-  pkgs.btop
-  pkgs.htop
-  pkgs.pipes
-  pkgs.pipes-rs
-  pkgs.asciiquarium-transparent
-  pkgs.eza
-  pkgs.lm_sensors
-  pkgs.tree
-  pkgs.picom
-  pkgs.rmpc
-  pkgs.git
-  pkgs.kde-rounded-corners
-  pkgs.python313Packages.cmake
-  pkgs.vim
-  pkgs.neovim
-  pkgs.nix-search-cli
-  pkgs.iotop
-  pkgs.bat
-  pkgs.sl
-  pkgs.superfile
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    kitty
+    fastfetch
+    fzf
+    cava
+    gnome-secrets
+    neofetch
+    starship
+    clock-rs
+    peaclock
+    btop
+    htop
+    pipes
+    pipes-rs
+    asciiquarium-transparent
+    eza
+    lm_sensors
+    tree
+    picom
+    rmpc
+    git
+    kde-rounded-corners
+    python313Packages.cmake
+    vim
+    neovim
+    nix-search-cli
+    iotop
+    bat
+    sl
+    superfile
   ];
-
 
   system.stateVersion = "25.11";
 }
