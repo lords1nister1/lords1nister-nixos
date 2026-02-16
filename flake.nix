1ac9a67558b12131a29c6b845ddc42deb63607f8
lords1nister1 <youubepro@gmail.com>
2026-02-16 17:04:14 +0100

    Added nvf-configuration.nix

--- flake.nix
+++ flake.nix
@@ -6,12 +6,12 @@
    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = {self, nixpkgs, nvf, ... }: {
+  outputs = { self, nixpkgs, nvf, ... }: {
 
   packages."x86_64-linux".default = 
    (nvf.lib.neovimConfiguration {
     pkgs = nixpkgs.legacyPackages."x86_64-linux";
-   
+    modules = [ ./nvf-configuration.nix ]; 
   }).neovim;
 
 
---
+++ nvf-configuration.nix
@@ -0,0 +1,10 @@
+{ pkgs, lib, ... }
+
+{
+ vim.theme.enable = true;
+ vim.theme.name = "gruvbox";
+ vim.theme.style= "dark";
+
+ vim.language.nix.enable = true;
+
+}
