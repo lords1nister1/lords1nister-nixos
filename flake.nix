ef6618ea62ed0e78e5382c2a53ef8e9c187472e4
lords1nister1 <youubepro@gmail.com>
2026-02-17 19:51:37 +0100

    Added spicetify

--- flake.nix
+++ flake.nix
@@ -2,18 +2,23 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nvf.url = "github:notashelf/nvf";
+    spicetify-nix.url = "github:Gerg-L/spicetify-nix";
   };
 
-  outputs = { self, nixpkgs, nvf, ... }:
-  let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
+  outputs = { self, nixpkgs, nvf, spicetify-nix, ... }:
+ let
+  system = "x86_64-linux";
+  pkgs = import nixpkgs {
+    inherit system;
+    config.allowUnfree = true;  # Enables unfree for all pkgs derived here
+  };
+  spicetify = spicetify-nix.lib.mkSpicetify pkgs { };  # Now uses unfree-enabled pkgs
 
-    neovimPkg = (nvf.lib.neovimConfiguration {
-      inherit pkgs;
-      modules = [ ./nvf-configuration.nix ];
-    }).neovim;
-  in
+  neovimPkg = (nvf.lib.neovimConfiguration {
+    inherit pkgs;
+    modules = [ ./nvf-configuration.nix ];
+  }).neovim;
+  in 
   {
     packages.${system}.default = neovimPkg;
 
@@ -22,9 +27,11 @@
       modules = [
         ./configuration.nix
         nvf.nixosModules.default
+        spicetify-nix.nixosModules.default
         {
           environment.systemPackages = [
             neovimPkg
+            spicetify
           ];
         }
       ];
