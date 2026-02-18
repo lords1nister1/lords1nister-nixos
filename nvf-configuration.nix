8aebfc82727dded45ba48fcc31c6330de05292dd
lords1nister1 <youubepro@gmail.com>
2026-02-18 19:21:16 +0100

    Attempted adding autopairs, failed

--- nvf-configuration.nix
+++ nvf-configuration.nix
@@ -1,5 +1,4 @@
 { pkgs, lib, ... }:
-
 {
   vim = {
     theme = {
@@ -10,10 +9,20 @@
 
     statusline.lualine.enable = true;
     telescope.enable = true;
+    autocomplete.nvim-cmp.enable = true;
 
-    autocomplete.nvim-cmp = {
-      enable = true;
-    };
+    optPlugins = [ "nvim-autopairs" ];
+
+    luaConfigRC.post = ''
+      -- Add extra pairing rule for Nix files
+      local ok_rule, rules = pcall(require, "nvim-autopairs.rules")
+      if ok_rule and type(rules.add_rules) == "function" then
+        local ok_Rule, Rule = pcall(require, "nvim-autopairs.rule")
+        if ok_Rule then
+          rules.add_rules({ Rule("{", "}", "nix") })
+        end
+      end
+    '';
 
     languages = {
       enableLSP = true;
